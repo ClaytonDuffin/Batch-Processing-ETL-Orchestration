027e4456cf3b9b1dccb51a8f62682d96c6ba56b6
Clayton Duffin <clayduffin@gmail.com>
2025-02-12 00:01:55 -0500

    Update Open-Meteo Hourly Data ETL Pipeline

--- OpenMeteoWeatherPipelineHourlyData.py
+++ OpenMeteoWeatherPipelineHourlyData.py
@@ -1,12 +1,12 @@
 from datetime import datetime, timedelta
 import numpy as np
+import pandas as pd
 import openmeteo_requests
 import requests_cache
 from retry_requests import retry
 
 # TODO
 # Add remaining 26 weather variables to parameters.
-# Refactor coordinateCycler to pull coordinates from curatedCoordinates.csv.
 # Clean, restructure, and sort responses.
 # Identify and implement transformations.
 # Initialize PostgreSQL tables. Insert to tables.
@@ -14,48 +14,54 @@ from retry_requests import retry
 
 def harvestWeatherData(latitude, longitude, startDate, endDate):
     
-    if not hasattr(harvestWeatherData, 'openMeteoClient'):
-        cacheSession = requests_cache.CachedSession('.cache', expire_after=-1)
-        retrySession = retry(cacheSession, retries=5, backoff_factor=0.2)
-        harvestWeatherData.openMeteoClient = openmeteo_requests.Client(session=retrySession)
+    try:       
+        if not hasattr(harvestWeatherData, 'openMeteoClient'):
+            cacheSession = requests_cache.CachedSession('.cache', expire_after=-1)
+            retrySession = retry(cacheSession, retries=5, backoff_factor=0.2)
+            harvestWeatherData.openMeteoClient = openmeteo_requests.Client(session=retrySession)
+    
+        openMeteo = harvestWeatherData.openMeteoClient
+        url = "https://archive-api.open-meteo.com/v1/archive"
+        
+        weatherVariables = ["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature"]
+        
+        params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "start_date": startDate,
+            "end_date": endDate,
+            "hourly": weatherVariables
+        }
+        
+        response = openMeteo.weather_api(url, params=params)
+        
+        return response
 
-    openMeteo = harvestWeatherData.openMeteoClient
-    url = "https://archive-api.open-meteo.com/v1/archive"
-    
-    weatherVariables = ["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature"]
-    
-    params = {
-        "latitude": latitude,
-        "longitude": longitude,
-        "start_date": startDate,
-        "end_date": endDate,
-        "hourly": weatherVariables
-    }
-    
-    response = openMeteo.weather_api(url, params=params)
-    
-    return response
+    except Exception as e:
+        raise Exception(f"Error occurred for latitude {latitude}, longitude {longitude}, in harvestWeatherData: {e}")
 
 
 def coordinateCycler():
     
-    coordinates = [{"latitude": 34.922183, "longitude": -85.67849},
-                   {"latitude": 32.93565, "longitude": -86.889836}]
-
-    sevenDaysAgo = (datetime.utcnow() - timedelta(days=7)).strftime("%Y-%m-%d")
-    sixDaysAgo = (datetime.utcnow() - timedelta(days=6)).strftime("%Y-%m-%d")
+    try:
+        curatedCoordinates = pd.read_csv("https://raw.githubusercontent.com/ClaytonDuffin/Batch-Processing-ETL-Orchestration/main/curatedCoordinates.csv")
     
-    responses = []
-    for coordinate in coordinates:
-        response = harvestWeatherData(coordinate['latitude'], coordinate['longitude'], sevenDaysAgo, sixDaysAgo)
-        responses.extend(response)
+        sevenDaysAgo = (datetime.utcnow() - timedelta(days=7)).strftime("%Y-%m-%d")
+        sixDaysAgo = (datetime.utcnow() - timedelta(days=6)).strftime("%Y-%m-%d")
     
-    return responses
+        responses = []
+        for _, location in curatedCoordinates.iterrows():
+            response = harvestWeatherData(location['Latitude'], location['Longitude'], sevenDaysAgo, sixDaysAgo)
+            responses.extend(response)
+        
+        return responses
+    
+    except Exception as e:
+        raise Exception(f"Unable to harvest Open-Meteo weather data. Error occurred in coordinateCycler: {e}")
 
 
 weatherAtCoordinates = coordinateCycler()
 
-for coordinate in weatherAtCoordinates:
-    medianTemperature = ((np.median(coordinate.Hourly().Variables(0).ValuesAsNumpy()) * (9/5)) + 32)
-    print(f"{coordinate.Latitude()}, {coordinate.Longitude()}, {medianTemperature}°F")
-
+for location in weatherAtCoordinates:
+    medianTemperature = ((np.median(location.Hourly().Variables(0).ValuesAsNumpy()) * (9/5)) + 32)
+    print(f"{location.Latitude()}, {location.Longitude()}, {medianTemperature}°F")
